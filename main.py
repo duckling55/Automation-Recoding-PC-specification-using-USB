68378ab37fb69cdf9d12c2dea737289576c5c7c5
Duckling <82367607+duckling55@users.noreply.github.com>
2021-04-19 15:35:09 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# import module
+import subprocess
+
+
+from datetime import datetime
+filename = f"Specification-{datetime.now():%Y-%m-%d %H-%M-%S}.txt"
+
+
+
+# traverse the info
+Id = subprocess.check_output(['systeminfo']).decode('utf-8').split('\n')
+new = []
+
+report = open(filename, 'w')
+# arrange the string into clear info
+for item in Id:
+    new.append(str(item.split("\r")[:-1]))
+for i in new:
+    report.write(i[2:-2]+'\n')
+report.close()
